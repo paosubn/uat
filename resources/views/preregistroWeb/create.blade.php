de3586f88d7cf42c0eda5789e244023bc3266101
Alfredo Viveros\FGE <aviveros@fiscaliaveracruz.gob.mx>
2018-03-08 14:08:23 -0600

    acomodo de archivos y correccion en formulario

---
+++ resources/views/preregistroWeb/create.blade.php
@@ -0,0 +1,90 @@
+
+@extends('template.main')
+@section('content')
+@if ($errors->any())
+	<div class="alert alert-danger">
+		<ul>
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
+@endif
+
+	
+	
+{!!Form::open(['route' => 'preregistro.store'])!!}
+	<div>
+		@include('preregistroWeb.fields.tipo-persona')
+	</div>
+	<div class="card" id="datosPer">
+		<div class="card-header">
+			<p class="lead" align="center">
+
+				Datos personales
+
+			</p>
+		</div>
+		<div id="collapsePersonales1" class="collapse show boxcollapse">
+			<div class="boxtwo">
+				@include('preregistroWeb.fields.datos-personales')
+			</div>
+		</div>
+	</div>
+
+	<!--div class="card" id="datosDir"-->
+		<div class="boxtwo">
+			<div class="form-group" align="center">
+				<div class="col">
+					<label class="col-form-label col-form-label-sm"  for="formGroupExampleInput">¿Con violencia?</label>
+					<div class="clearfix"></div>
+					<div class="form-check form-check-inline">
+						<label class="form-check-label col-form-label col-form-label-sm">
+							<input class="form-check-input" type="radio" id="conViolencia" name="conViolencia" value="1"> Sí
+						</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<label class="form-check-label col-form-label col-form-label-sm">
+							<input class="form-check-input" type="radio" id="sinViolencia" name="sinViolencia" value="0"> No
+						</label>
+					</div>
+				</div>
+			</div>
+		</div>
+	<!--/div-->
+
+
+
+	<div class="form-group">
+		<div class="col-12">
+			<div class="col">
+				<label for="narracion" class="col-form-label-sm">Narración</label>
+				<textarea name="narracion" id="" cols="30" rows="10" class="form-control form-control-sm" required=>
+					
+				</textarea>
+				
+			</div>
+		</div>
+	</div>
+	<!--BOTTONS-->
+	<div class="boxtwo">
+		<div class="row">
+			<div class="col">
+				<div class="text-left">
+					<a href="{{-- route('carpeta', $idCarpeta) --}}" class="btn btn-dark text-center">Volver atrás</a>
+				</div>
+			</div>
+			<div class="col">   
+				<div class="text-right">
+						<a href="{!! route('home') !!}" title="" class="btn btn-danger">Cancelar</a>
+						{{ Form::submit('Guardar', array('class' => 'btn btn-success')) }}
+				
+				</div>
+			</div>
+		</div>
+	</div>
+
+{!!Form::close()!!}
+
+
+@endsection
--- resources/views/preregistroWeb/fields/datos-personales.blade.php
+++ resources/views/preregistroWeb/fields/datos-personales.blade.php
@@ -1,6 +1,7 @@
 <div class="row">
 	<div class="col-12" id="personaFisica">
 		<div class="row">
+			
 			<div class="col-4">
 				<div class="form-group">
 					{!! Form::label('nombres', 'Nombre', ['class' => 'col-form-label-sm','valid-tooltip']) !!}
@@ -8,6 +9,7 @@
 					<div class="help-block with-errors"></div> 
 				</div>
 			</div>
+			
 			<div class="col-4">
 				<div class="form-group">
 					{!! Form::label('primerAp', 'Primer apellido', ['class' => 'col-form-label-sm']) !!}
@@ -15,12 +17,14 @@
 					<div class="help-block with-errors"></div>
 				</div>
 			</div>
+			
 			<div class="col-4">
 				<div class="form-group">
 					{!! Form::label('segundoAp', 'Segundo apellido', ['class' => 'col-form-label-sm']) !!}
 					{!! Form::text('segundoAp', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ingrese el segundo apellido']) !!}
 				</div>
 			</div>
+			
 			<div class="col-4">
 				<div class="form-group">
 					{!! Form::label('rfc', 'R.F.C.', ['class' => 'col-form-label-sm']) !!}
@@ -28,43 +32,44 @@
 					<div class="help-block with-errors"></div>
 				</div>
 			</div>
-				<div class="col-4">
+			
+			<div class="col-4">
 				<div class="form-group">
 					{!! Form::label('fechaNacimiento', 'Fecha de nacimiento', ['class' => 'col-form-label-sm']) !!}
 					<div class="input-group date" id="fechanac" data-target-input="nearest">
-		                {!! Form::text('fechaNacimiento', null, ['class' => 'form-control form-control-sm datetimepicker-input', 'data-target' => '#fechanac', 'required', 'placeholder' => 'DD/MM/AAAA','required']) !!}
-		                <span class="input-group-addon" data-target="#fechanac" data-toggle="datetimepicker">
-		                    <i class="fa fa-calendar" aria-hidden="true"></i>
+						{!! Form::text('fechaNacimiento', null, ['class' => 'form-control form-control-sm datetimepicker-input', 'data-target' => '#fechanac', 'required', 'placeholder' => 'DD/MM/AAAA','required']) !!}
+						<span class="input-group-addon" data-target="#fechanac" data-toggle="datetimepicker">
+							<i class="fa fa-calendar" aria-hidden="true"></i>
 
 
-		                </span>
+						</span>
 
-		            </div>
+					</div>
 					<div class="help-block with-errors"></div>
 				</div>
 			</div>
 
 			<div class="col-4">
-		<div class="row">
-			<div class="col-6">
-				<div class="form-group">
-					{!! Form::label('edad', 'Edad', ['class' => 'col-form-label-sm']) !!}
-					{!! Form::number('edad', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ingrese la edad', 'min' => 0, 'max' => 150, 'required']) !!}
-					<div class="help-block with-errors"></div>
-				</div>
-
-			</div>
-			<div class="col-6">
-				<div class="form-group">
-					{!! Form::label('sexo', 'Sexo', ['class' => 'col-form-label-sm']) !!}
-						{!! Form::select('sexo', ['SIN INFORMACION' => 'SIN INFORMACION', 'HOMBRE' => 'HOMBRE', 'MUJER' => 'MUJER'], null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Seleccione el sexo', 'required']) !!}
-						<div class="help-block with-errors"></div>
+				
+				<div class="row">
+					<div class="col-6">
+						<div class="form-group">
+							{!! Form::label('edad', 'Edad', ['class' => 'col-form-label-sm']) !!}
+							{!! Form::number('edad', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ingrese la edad', 'min' => 0, 'max' => 150, 'required']) !!}
+							<div class="help-block with-errors"></div>
+						</div>
+	
+					</div>
+					<div class="col-6">
+						<div class="form-group">
+							{!! Form::label('sexo', 'Sexo', ['class' => 'col-form-label-sm']) !!}
+							{!! Form::select('sexo', ['SIN INFORMACION' => 'SIN INFORMACION', 'HOMBRE' => 'HOMBRE', 'MUJER' => 'MUJER'], null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Seleccione el sexo', 'required']) !!}
+							<div class="help-block with-errors"></div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 
-   
 			<div class="col-4">
 				<div class="form-group">
 					{!! Form::label('curp', 'C.U.R.P.', ['class' => 'col-form-label-sm']) !!}
@@ -80,8 +85,6 @@
 				</div>
 			</div>
 
-
-
 			<div class="col-4">
 				<div class="form-group">
 					{!! Form::label('docIdentificacion', 'Documento de identificación', ['class' => 'col-form-label-sm']) !!}
@@ -89,6 +92,7 @@
 					<div class="help-block with-errors"></div>
 				</div>
 			</div>
+	
 			<div class="col-4">
 				<div class="form-group">
 					{!! Form::label('numDocIdentificacion', 'Núm. de documento de identificación', ['class' => 'col-form-label-sm']) !!}
@@ -96,32 +100,36 @@
 					<div class="help-block with-errors"></div>
 				</div>
 			</div>
+	
 			<div class="col-4">
-			<div class="form-group">
-			{!! Form::label('idLocalidad', 'Localidad', ['class' => 'col-form-label-sm']) !!}
-			{!! Form::select('idLocalidad', [ '' => 'Seleccione una localidad'], null, ['class' => 'form-control form-control-sm', 'required']) !!}
-			<div class="help-block with-errors"></div>
+				<div class="form-group">
+					{!! Form::label('idLocalidad', 'Localidad', ['class' => 'col-form-label-sm']) !!}
+					{!! Form::select('idLocalidad', [ '' => 'Seleccione una localidad'], null, ['class' => 'form-control form-control-sm']) !!}
+					<div class="help-block with-errors"></div>
 				</div>
 			</div>
 	<!--cambiar por larabelCollective-->
 				
 			<div class="col-4">
-		<div class="form-group">
-			{!! Form::label('idMunicipio', 'Municipio', ['class' => 'col-form-label-sm']) !!}
-			{!! Form::select('idMunicipio', [''=>'Seleccione un municipio'], null, ['class' => 'form-control form-control-sm', 'required']) !!}
-			<div class="help-block with-errors"></div>
-		</div>
-	</div>
-	<div class="col-4">
-				<div class="form-group col-form-label-sm">
-					<label for="sel1">Entidad Federativa:</label>
-  						<select class="form-control" id="sel1">
-						    <option>1</option>
-						    <option>2</option>
-						    <option>3</option>
-						    <option>4</option>
-						 </select>
+				<div class="form-group">
+					{!! Form::label('idMunicipio', 'Municipio', ['class' => 'col-form-label-sm']) !!}
+					{!! Form::select('idMunicipio', [''=>'Seleccione un municipio'], null, ['class' => 'form-control form-control-sm' ]) !!}
+					<div class="help-block with-errors"></div>
 				</div>
 			</div>
 	
+			<div class="col-4">
+				<div class="form-group col-form-label-sm">
+					<label for="sel1">Entidad Federativa:</label>
+						<select class="form-control" id="sel1">
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+						</select>
+				</div>
+			</div>
+
+		</div>
 	</div>
+</div>
